fcca2a4091e213310fd00bf81d5299e7c937b15f
Ferhuce <100046579+Ferhunt@users.noreply.github.com>
2023-08-15 13:03:48 -0700

    added gopls server to lsp

--- init.lua
+++ init.lua
@@ -338,7 +338,7 @@ end
 --  define the property 'filetypes' to the map in question.
 local servers = {
   -- clangd = {},
-  -- gopls = {},
+  gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
   -- tsserver = {},
